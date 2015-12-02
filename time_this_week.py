7e57e1a0ac0a61da02a7e4ecea7f4c2a1f216b92
Robert Gardner <rhg259@nyu.edu>
2015-12-02 17:11:55 -0500

    Add jinyang timeweek script

---
+++ time_this_week.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Prints hours worked this week and hours remaining.
+
+Accepts csv of hours from stdin.
+month/day, start, end
+
+"""
+
+from __future__ import print_function
+from datetime import datetime
+import sys
+
+# The number of hours you want to work in a given week.
+TARGET_HOURS_PER_WEEK = 35.0
+
+THIS_YEAR = datetime.today().year
+
+def parse_time(time):
+    if ":" in time:
+        return datetime.strptime(time, "%H:%M")
+    else:
+        return datetime.strptime(time, "%H")
+
+if len(sys.argv) != 2:
+    print("time_this_week.py <hours.csv>")
+    sys.exit(1)
+
+# Get today's day and month
+this_week = datetime.now().isocalendar()[1]
+
+total_minutes = 0
+with open(sys.argv[1]) as input_file:
+    # Skip the header row in the file.
+    input_file.readline()
+
+    for line in input_file:
+        data = line.strip().split(',')
+        if len(data) != 3:
+            # Ensure valid data.
+            continue
+
+        date, start, end = data
+
+        # Parse date
+        month, day = date.split('/')
+        date = datetime(THIS_YEAR, int(month), int(day))
+        if date.isocalendar()[1] != this_week:
+            # Skip data not from this work week.
+            continue
+
+        minutes = (parse_time(end) - parse_time(start)).total_seconds() / 60.0
+        total_minutes += minutes
+
+hours_worked = total_minutes / 60.0
+percent_worked = (hours_worked / TARGET_HOURS_PER_WEEK) * 100
+
+print("%1.2f hours worked this week. (%d%%)" % (hours_worked, percent_worked))
+print("%1.2f hours remaining." % (TARGET_HOURS_PER_WEEK - hours_worked))
