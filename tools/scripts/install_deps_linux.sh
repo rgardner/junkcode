5f4078ff7c5643516bd79021bb3df42f2324c14b
Bob Gardner <bob.hn.gardner@gmail.com>
2019-11-07 20:54:44 -0500

    Install clang-tidy earlier

--- tools/scripts/install_deps_linux.sh
+++ tools/scripts/install_deps_linux.sh
@@ -9,12 +9,11 @@ sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt-get update
 
 sudo apt-get install --yes --no-install-recommends \
   build-essential \
+  clang-tidy \
   ninja-build \
   python3.7 \
   python3.7-dev \
   python3.7-venv
-
-sudo apt-get upgrade --yes
 
 if [ ! -d "${REPO_ROOT}/.venv/bin/activate" ]; then
   python3.7 -m venv "${REPO_ROOT}/.venv"
